5414942b0c458355ecc15fc313f40b1df73983cd
Pete <p.wallroth@yahoo.com>
2023-12-13 12:15:25 +1100

    13Dec23-1215

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function square(number) {
+    return number + number;
+}
+//asdfs
+console.log(square(2342343));
+
+document.querySelector('#button').addEventListener('click', function () {
+    let askTime = prompt("Please provide the time:");
+    return null;
+});
+
+
+
+function testLogicalAnd(number) {
+  // Only change code below this line
+
+  if (number <= 50 && number >= 25) {
+      return "Fuck Yes";
+  }
+
+  return "Fuck No";
+}
+
+console.log(testLogicalAnd(50))
