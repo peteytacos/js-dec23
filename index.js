ff2af55444fc7da104198934d1c6ac2fca954e6a
Pete <p.wallroth@yahoo.com>
2023-12-19 11:51:58 +1100

    19dec23_1152

--- index.js
+++ index.js
@@ -9,17 +9,3 @@ document.querySelector('#button').addEventListener('click', function () {
     let askTime = prompt("Please provide the time:");
     return null;
 });
-
-
-
-function testLogicalAnd(number) {
-  // Only change code below this line
-
-  if (number <= 50 && number >= 25) {
-      return "Fuck Yes";
-  }
-
-  return "Fuck No";
-}
-
-console.log(testLogicalAnd(50))
